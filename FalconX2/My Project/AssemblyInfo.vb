f7e8ef04cc8231e81367ce781fd763f41ef04ca8
Chris Andriessen <50437199+ChrisAnd1998@users.noreply.github.com>
2019-07-11 11:04:48 +0200

    Add files via upload

--- FalconX2/My Project/AssemblyInfo.vb
+++ FalconX2/My Project/AssemblyInfo.vb
@@ -32,6 +32,6 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.0.9.0")>
-<Assembly: AssemblyFileVersion("1.0.9.0")>
+<Assembly: AssemblyVersion("1.1.0.0")>
+<Assembly: AssemblyFileVersion("1.1.0.0")>
 <Assembly: NeutralResourcesLanguage("en")>
