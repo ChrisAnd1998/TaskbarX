db1508daf18c9dc70bc66b66f18bbc24cf28d6b7
Chris Andriessen <50437199+ChrisAnd1998@users.noreply.github.com>
2019-07-13 12:55:32 +0200

    Add files via upload

---
+++ FalconX3/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("FalconX3")>
+<Assembly: AssemblyDescription("FalconX centers your Taskbar icons.")>
+<Assembly: AssemblyCompany("Chris Andriessen")>
+<Assembly: AssemblyProduct("FalconX3")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("577b2dfc-0055-430b-a159-a3cef2cb1f78")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.1.2.0")>
+<Assembly: AssemblyFileVersion("1.1.2.0")>
