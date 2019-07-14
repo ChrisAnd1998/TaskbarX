fa5e11c466fb577885815e9eae3da91f6c02e1e1
Chris Andriessen <50437199+ChrisAnd1998@users.noreply.github.com>
2019-07-14 12:52:47 +0200

    Add files via upload

--- FalconX3/My Project/Settings.Designer.vb
+++ FalconX3/My Project/Settings.Designer.vb
@@ -161,6 +161,18 @@ Namespace My
                 Me("runatstartup") = value
             End Set
         End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("False")>  _
+        Public Property s_centerbetweentoolbars() As Boolean
+            Get
+                Return CType(Me("s_centerbetweentoolbars"),Boolean)
+            End Get
+            Set
+                Me("s_centerbetweentoolbars") = value
+            End Set
+        End Property
     End Class
 End Namespace
 
