f882bfa101092df92c4ec5574c6c48f21fa93219
Chris Andriessen <50437199+ChrisAnd1998@users.noreply.github.com>
2019-09-20 21:57:01 +0200

    Add files via upload

--- FalconX3/My Project/Settings.Designer.vb
+++ FalconX3/My Project/Settings.Designer.vb
@@ -15,7 +15,7 @@ Option Explicit On
 Namespace My
     
     <Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(),  _
-     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.1.0.0"),  _
+     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.2.0.0"),  _
      Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)>  _
     Partial Friend NotInheritable Class MySettings
         Inherits Global.System.Configuration.ApplicationSettingsBase
@@ -171,6 +171,18 @@ Namespace My
             End Get
             Set
                 Me("s_centerbetweentoolbars") = value
+            End Set
+        End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("400")>  _
+        Public Property s_react() As Decimal
+            Get
+                Return CType(Me("s_react"),Decimal)
+            End Get
+            Set
+                Me("s_react") = value
             End Set
         End Property
     End Class
