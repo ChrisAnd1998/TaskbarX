8756ea7fb3a230f8f69e754ec83486eadaf38777
Chris Andriessen <50437199+ChrisAnd1998@users.noreply.github.com>
2019-10-09 13:58:09 +0200

    Add files via upload

--- FalconX3/My Project/Settings.Designer.vb
+++ FalconX3/My Project/Settings.Designer.vb
@@ -15,7 +15,7 @@ Option Explicit On
 Namespace My
     
     <Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(),  _
-     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.2.0.0"),  _
+     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.3.0.0"),  _
      Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)>  _
     Partial Friend NotInheritable Class MySettings
         Inherits Global.System.Configuration.ApplicationSettingsBase
@@ -183,6 +183,18 @@ Namespace My
             End Get
             Set
                 Me("s_react") = value
+            End Set
+        End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("")>  _
+        Public Property anima() As String
+            Get
+                Return CType(Me("anima"),String)
+            End Get
+            Set
+                Me("anima") = value
             End Set
         End Property
     End Class
