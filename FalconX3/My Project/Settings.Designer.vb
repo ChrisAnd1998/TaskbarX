40f7357dafa5d645579378cd58d8a3cd51f8bed2
Chris Andriessen <50437199+ChrisAnd1998@users.noreply.github.com>
2019-10-12 22:25:27 +0200

    Add files via upload

--- FalconX3/My Project/Settings.Designer.vb
+++ FalconX3/My Project/Settings.Designer.vb
@@ -197,6 +197,18 @@ Namespace My
                 Me("anima") = value
             End Set
         End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("False")>  _
+        Public Property multi() As Boolean
+            Get
+                Return CType(Me("multi"),Boolean)
+            End Get
+            Set
+                Me("multi") = value
+            End Set
+        End Property
     End Class
 End Namespace
 
