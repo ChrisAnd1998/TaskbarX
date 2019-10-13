71c29b0350312b41f345b686244e3cc17e9d2400
Chris Andriessen <50437199+ChrisAnd1998@users.noreply.github.com>
2019-10-13 07:56:31 +0200

    Add files via upload

--- FalconX3/My Project/Application.Designer.vb
+++ FalconX3/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.FalconX3.Form1
+            Me.MainForm = Global.FalconX3.FalconX
         End Sub
     End Class
 End Namespace
