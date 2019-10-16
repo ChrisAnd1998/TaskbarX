9a64f8a3e65142f26517c5ce9d5c2038268746ad
Chris Andriessen <50437199+ChrisAnd1998@users.noreply.github.com>
2019-10-16 19:20:05 +0200

    Add files via upload

--- FalconX3/Effects/Bounds/XLocationEffect.vb
+++ FalconX3/Effects/Bounds/XLocationEffect.vb
@@ -1,1 +1,36 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows.Forms
 
+Namespace VisualEffects.Animations.Effects
+
+    Public Class XLocationEffect
+        Implements IEffect
+
+        Public Function GetCurrentValue(ByVal control As Control) As Integer Implements IEffect.GetCurrentValue
+            Return control.Left
+        End Function
+
+        Public Sub SetValue(ByVal control As Control, ByVal originalValue As Integer, ByVal valueToReach As Integer, ByVal newValue As Integer) Implements IEffect.SetValue
+            control.Left = newValue
+        End Sub
+
+        Public Function GetMinimumValue(ByVal control As Control) As Integer Implements IEffect.GetMinimumValue
+            Return Int32.MinValue
+        End Function
+
+        Public Function GetMaximumValue(ByVal control As Control) As Integer Implements IEffect.GetMaximumValue
+            Return Int32.MaxValue
+        End Function
+
+        Public ReadOnly Property IEffect_Interaction As EffectInteractions Implements IEffect.Interaction
+            Get
+                Return EffectInteractions.X
+            End Get
+        End Property
+
+    End Class
+
+End Namespace
