4a122cf217778ca8a1543e29f7b1c1cb65d25faa
Chris Andriessen <50437199+ChrisAnd1998@users.noreply.github.com>
2019-10-16 19:21:02 +0200

    Add files via upload

--- FalconX3/Extensions/AnimationExtensions.vb
+++ FalconX3/Extensions/AnimationExtensions.vb
@@ -1,1 +1,20 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows.Forms
+Imports System.Threading
 
+Imports FalconX3.VisualEffects.Easing
+Imports FalconX3.VisualEffects.Animators
+
+Namespace VisualEffects
+    Public Module AnimationExtensions
+
+        <System.Runtime.CompilerServices.Extension>
+        Public Function Animate(ByVal control As Control, ByVal iAnimation As IEffect, ByVal easing As EasingDelegate, ByVal valueToReach As Integer, ByVal duration As Integer, ByVal delay As Integer, Optional ByVal reverse As Boolean = False, Optional ByVal loops As Integer = 1) As AnimationStatus
+            Return Animator.Animate(control, iAnimation, easing, valueToReach, duration, delay, reverse, loops)
+        End Function
+
+    End Module
+End Namespace
