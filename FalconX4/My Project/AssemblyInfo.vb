1f81be742c6e444de72c0996789ba6ddd9d04ef3
Chris Andriessen <50437199+ChrisAnd1998@users.noreply.github.com>
2019-10-20 11:05:06 +0200

    Add files via upload

--- FalconX4/My Project/AssemblyInfo.vb
+++ FalconX4/My Project/AssemblyInfo.vb
@@ -1,1 +1,36 @@
+﻿Imports System.Resources
+Imports System.Reflection
+Imports System.Runtime.InteropServices
 
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("FalconX")>
+<Assembly: AssemblyDescription("Automated taskbar centering.")>
+<Assembly: AssemblyCompany("Chris Andriessen")>
+<Assembly: AssemblyProduct("FalconX")>
+<Assembly: AssemblyCopyright("Copyright © Chris Andriessen 2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exPositioned to COM
+<Assembly: Guid("5cad66db-1e3d-4aae-b56c-ed060b09d4ff")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.2.8.0")>
+<Assembly: AssemblyFileVersion("1.2.8.0")>
+<Assembly: NeutralResourcesLanguage("en")>
