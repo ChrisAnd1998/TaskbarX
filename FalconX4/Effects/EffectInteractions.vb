d2b868bdd8d5c8e822656aef41350e6baf00634e
Chris Andriessen <50437199+ChrisAnd1998@users.noreply.github.com>
2019-10-20 11:06:34 +0200

    Add files via upload

--- FalconX4/Effects/EffectInteractions.vb
+++ FalconX4/Effects/EffectInteractions.vb
@@ -1,1 +1,16 @@
+﻿Namespace VisualEffects.Animations.Effects
 
+    <Flags>
+    Public Enum EffectInteractions
+        X = 1
+        Y = 2
+        WIDTH = 8
+        HEIGHT = 4
+        COLOR = 16
+        TRANSPARENCY = 32
+        LOCATION = X Or Y
+        SIZE = WIDTH Or HEIGHT
+        BOUNDS = X Or Y Or WIDTH Or HEIGHT
+    End Enum
+
+End Namespace
