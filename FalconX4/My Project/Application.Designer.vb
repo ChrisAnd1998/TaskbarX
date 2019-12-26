1fd31b608368e25026763ab548b88f18a5e48691
Chris Andriessen <50437199+ChrisAnd1998@users.noreply.github.com>
2019-12-26 15:08:12 +0100

    Add files via upload

--- FalconX4/My Project/Application.Designer.vb
+++ FalconX4/My Project/Application.Designer.vb
@@ -11,28 +11,3 @@
 Option Strict On
 Option Explicit On
 
-
-Namespace My
-    
-    'NOTE: This file is auto-generated; do not modify it directly.  To make changes,
-    ' or if you encounter build errors in this file, go to the Project Designer
-    ' (go to Project Properties or double-click the My Project node in
-    ' Solution Explorer), and make changes on the Application tab.
-    '
-    Partial Friend Class MyApplication
-        
-        <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
-        Public Sub New()
-            MyBase.New(Global.Microsoft.VisualBasic.ApplicationServices.AuthenticationMode.Windows)
-            Me.IsSingleInstance = true
-            Me.EnableVisualStyles = true
-            Me.SaveMySettingsOnExit = false
-            Me.ShutDownStyle = Global.Microsoft.VisualBasic.ApplicationServices.ShutdownMode.AfterMainFormCloses
-        End Sub
-        
-        <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
-        Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.FalconX4.Gui
-        End Sub
-    End Class
-End Namespace
