a9f6f857d1627de7a06d2075e687c6db5acb1f6e
Chris Andriessen <50437199+ChrisAnd1998@users.noreply.github.com>
2020-01-05 10:32:30 +0100

    Add files via upload

--- FalconX4/My Project/AssemblyInfo.vb
+++ FalconX4/My Project/AssemblyInfo.vb
@@ -1,5 +1,5 @@
-﻿Imports System.Resources
-Imports System.Reflection
+﻿Imports System.Reflection
+Imports System.Resources
 Imports System.Runtime.InteropServices
 
 ' General Information about an assembly is controlled through the following
@@ -31,6 +31,6 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.3.7.0")>
-<Assembly: AssemblyFileVersion("1.3.7.0")>
+<Assembly: AssemblyVersion("1.3.8.0")>
+<Assembly: AssemblyFileVersion("1.3.8.0")>
 <Assembly: NeutralResourcesLanguage("en")>
