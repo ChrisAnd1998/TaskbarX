30ecf00fc783816e862d262dd286221e54f3a3e2
Chris Andriessen <50437199+ChrisAnd1998@users.noreply.github.com>
2020-04-13 16:22:39 +0200

    Add files via upload

--- TaskbarX/packages/UIAComWrapperX.1.1.0.16/tools/install.ps1
+++ TaskbarX/packages/UIAComWrapperX.1.1.0.16/tools/install.ps1
@@ -1,1 +1,13 @@
+param($installPath, $toolsPath, $package, $project)
 
+$frameworkVersionString = $project.Properties.Item('TargetFrameworkMoniker').Value
+$frameworkVersionString -match 'Version=(v\d\.\d)'
+$frameworkVersion = $matches[1]
+$project.Save();
+
+if ($frameworkVersion -ge 'v4.0')
+{
+    $project.Object.References | Where-Object { $_.EmbedInteropTypes -eq $true -and $_.Name -eq "Interop.UIAutomationClient" } | ForEach-Object { $_.EmbedInteropTypes = $false }
+}
+
+$project.Save()
