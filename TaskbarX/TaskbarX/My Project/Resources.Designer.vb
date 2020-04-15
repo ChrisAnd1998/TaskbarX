8415898924d6b1b6a38142a42e3e06f1458a069b
Chris Andriessen <50437199+ChrisAnd1998@users.noreply.github.com>
2020-04-15 18:56:44 +0200

    Add files via upload

--- TaskbarX/TaskbarX/My Project/Resources.Designer.vb
+++ TaskbarX/TaskbarX/My Project/Resources.Designer.vb
@@ -39,7 +39,7 @@ Namespace My.Resources
         Friend ReadOnly Property ResourceManager() As Global.System.Resources.ResourceManager
             Get
                 If Object.ReferenceEquals(resourceMan, Nothing) Then
-                    Dim temp As Global.System.Resources.ResourceManager = New Global.System.Resources.ResourceManager("FalconX5.Resources", GetType(Resources).Assembly)
+                    Dim temp As Global.System.Resources.ResourceManager = New Global.System.Resources.ResourceManager("TaskbarX.Resources", GetType(Resources).Assembly)
                     resourceMan = temp
                 End If
                 Return resourceMan
