55aacee616a2b74d3e20de5c1f5b4434797eed6b
Chris Andriessen <50437199+ChrisAnd1998@users.noreply.github.com>
2020-04-20 21:35:06 +0200

    Add files via upload

--- TaskbarX/TaskbarX/My Project/AssemblyInfo.vb
+++ TaskbarX/TaskbarX/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.5.3.0")>
-<Assembly: AssemblyFileVersion("1.5.3.0")>
+<Assembly: AssemblyVersion("1.5.4.0")>
+<Assembly: AssemblyFileVersion("1.5.4.0")>
