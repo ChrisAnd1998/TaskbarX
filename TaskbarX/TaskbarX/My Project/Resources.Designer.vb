638252519d2f4bc6f6e33280cbc011ac9018c186
Chris Andriessen <50437199+ChrisAnd1998@users.noreply.github.com>
2020-08-18 17:52:10 +0200

    Add files via upload

--- TaskbarX/TaskbarX/My Project/Resources.Designer.vb
+++ TaskbarX/TaskbarX/My Project/Resources.Designer.vb
@@ -59,5 +59,15 @@ Namespace My.Resources
                 resourceCulture = value
             End Set
         End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        '''</summary>
+        Friend ReadOnly Property icon() As System.Drawing.Icon
+            Get
+                Dim obj As Object = ResourceManager.GetObject("icon", resourceCulture)
+                Return CType(obj,System.Drawing.Icon)
+            End Get
+        End Property
     End Module
 End Namespace
