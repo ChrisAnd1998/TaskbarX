cf40bb33c7eca5cbd309ba722a0d7f81e1461626
Chris Andriessen <50437199+ChrisAnd1998@users.noreply.github.com>
2020-08-27 08:47:36 +0200

    Add files via upload

--- TaskbarX Configurator/TaskbarX Configurator/My Project/AssemblyInfo.vb
+++ TaskbarX Configurator/TaskbarX Configurator/My Project/AssemblyInfo.vb
@@ -1,1 +1,59 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+Imports System.Globalization
+Imports System.Resources
+Imports System.Windows
 
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("TaskbarX Configurator")>
+<Assembly: AssemblyDescription("Configurator for TaskbarX")>
+<Assembly: AssemblyCompany("Chris Andriessen")>
+<Assembly: AssemblyProduct("TaskbarX Configurator")>
+<Assembly: AssemblyCopyright("Copyright © Chris Andriessen 2020")>
+<Assembly: AssemblyTrademark("")>
+<Assembly: ComVisible(false)>
+
+'In order to begin building localizable applications, set
+'<UICulture>CultureYouAreCodingWith</UICulture> in your .vbproj file
+'inside a <PropertyGroup>.  For example, if you are using US english
+'in your source files, set the <UICulture> to "en-US".  Then uncomment the
+'NeutralResourceLanguage attribute below.  Update the "en-US" in the line
+'below to match the UICulture setting in the project file.
+
+'<Assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)>
+
+
+'The ThemeInfo attribute describes where any theme specific and generic resource dictionaries can be found.
+'1st parameter: where theme specific resource dictionaries are located
+'(used if a resource is not found in the page,
+' or application resource dictionaries)
+
+'2nd parameter: where the generic resource dictionary is located
+'(used if a resource is not found in the page,
+'app, and any theme specific resource dictionaries)
+<Assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)>
+
+
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("1d3ba368-75e1-42b7-910d-e581ebd0da78")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.5.9.0")>
+<Assembly: AssemblyFileVersion("1.5.9.0")>
