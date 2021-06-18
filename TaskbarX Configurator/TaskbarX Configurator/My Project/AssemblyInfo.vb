7b060d523cd115ad27291b03f62b8a9bf03506e9
Chris Andriessen <50437199+ChrisAnd1998@users.noreply.github.com>
2021-06-18 20:40:31 +0200

    Add files via upload

--- TaskbarX Configurator/TaskbarX Configurator/My Project/AssemblyInfo.vb
+++ TaskbarX Configurator/TaskbarX Configurator/My Project/AssemblyInfo.vb
@@ -55,5 +55,5 @@ Imports System.Windows
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.6.9.0")>
-<Assembly: AssemblyFileVersion("1.6.9.0")>
+<Assembly: AssemblyVersion("1.7.0.0")>
+<Assembly: AssemblyFileVersion("1.7.0.0")>
