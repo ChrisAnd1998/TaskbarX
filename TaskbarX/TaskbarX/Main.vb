4e7b3b74e4dacd843ed30dd5ef9988749dfcecce
Chris Andriessen <50437199+ChrisAnd1998@users.noreply.github.com>
2021-12-26 10:07:34 +0100

    Fix #717

--- TaskbarX/TaskbarX/Main.vb
+++ TaskbarX/TaskbarX/Main.vb
@@ -307,7 +307,8 @@ Public Class Main
 
             Dim tt As New Win32.RECT
             Win32.GetClientRect(trayptr, tt)
-            Win32.SetWindowRgn(CType(trayptr, IntPtr), Win32.CreateRoundRectRgn(-1, -1, tt.Right + 1, tt.Bottom - tt.Top + 1, -1, -1), True)
+            ''Win32.SetWindowRgn(CType(trayptr, IntPtr), Win32.CreateRoundRectRgn(-1, -1, tt.Right + 1, tt.Bottom - tt.Top + 1, -1, -1), True)
+            Win32.SetWindowRgn(CType(trayptr, IntPtr), Win32.CreateRectRgn(tt.Left, tt.Top, tt.Right, tt.Bottom), True)
 
 
 
